4a5840ddeae3ec8ce85de98fe93c8c3300c5d063
Paula Lopes <contato@paulalopes.com.br>
2024-10-29 18:47:09 -0300

    feat: add refetch action to refresh admissions in Searchbar

--- src/pages/Dashboard/components/Searchbar/index.tsx
+++ src/pages/Dashboard/components/Searchbar/index.tsx
@@ -1,22 +1,30 @@
 import Button from "@/components/Buttons";
 import { IconButton } from "@/components/Buttons/IconButton";
 import TextField from "@/components/TextField";
+import { useAdmission } from "@/context/AdmissionContext";
 import routes from "@/router/routes";
 import { HiRefresh } from "react-icons/hi";
 import { useHistory } from "react-router-dom";
 import * as S from "./styles";
+
 export const SearchBar = () => {
+  const { fetchAdmissions } = useAdmission();
+
   const history = useHistory();
 
   const goToNewAdmissionPage = () => {
     history.push(routes.newUser);
   };
 
+  const handleRefetch = () => {
+    fetchAdmissions();
+  };
+
   return (
     <S.Container>
       <TextField placeholder="Digite um CPF válido" />
       <S.Actions>
-        <IconButton aria-label="refetch">
+        <IconButton aria-label="refetch" onClick={handleRefetch}>
           <HiRefresh />
         </IconButton>
         <Button onClick={() => goToNewAdmissionPage()}>Nova Admissão</Button>
